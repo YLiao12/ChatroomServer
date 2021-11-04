6d8230a199ac8182e4c266f85aeab810d374b887
YLiao12 <seu_lyq@outlook.com>
2021-11-04 16:16:32 +0800

    connected to local mysql, haven't finished the json output

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
+from flask.wrappers import Request
 import mysql.connector
-import json
-from flask import Flask
+from flask import Flask, request, jsonify, json
     
 app = Flask(__name__)
 
-# mydb = mysql.connector.connect(
-#   host="18.219.7.155",       # 数据库主机地址
-#   user="leo",    # 数据库用户名
-#   passwd="L21516114leo"   # 数据库密码
-# )
+conn = mysql.connector.connect(
+  host="localhost",       # 数据库主机地址
+  user="dbuser",    # 数据库用户名
+  passwd="password",   # 数据库密码
+  database = "iems5722",
+)
+cursor = conn.cursor(dictionary = True)
 
 @app.route("/") 
 def hello_world():
@@ -19,20 +21,43 @@ def get_chatrooms():
     # get records from mysql
     # change records into json
     # return json
-    pass
+    query_chatroom = "SELECT * FROM chatrooms ORDER BY id ASC"
+    cursor.execute(query_chatroom)
+    results = cursor.fetchall()
+    jsonResult = json.dumps(results)
+    # some issues about json 
+    return jsonify(status="OK", data=jsonResult)
+    # return jsonResult
 
-@app.route("/api/a3/get_messages")
+@app.route("/api/a3/get_messages", methods=["GET"])
 def get_messages():
     # get chatroom_id and page from the api
     # get message from mysql
-    # change into json. return
+    # change into json. return\
+    chatroom_id = request.args.get("chatroom_id")
+    page = request.args.get("page")
+    queryMessages = "select * from messages where chatroom_id = %s"
+    param = (chatroom_id, )
+    cursor.execute(queryMessages, param)
+    results = cursor.fetchall()
+    return json.dumps(results)
     pass
 
-@app.route("/api/a3/send_message")
+@app.route("/api/a3/send_message", methods=["POST"])
 def send_message():
     # get chatroom_id, user_id, name, message from post api
     # write into mysql
     # return status OK json
+    chatroom_id = request.form.get("chatroom_id")
+    user_id = request.form.get("user_id")
+    name = request.form.get("name")
+    message = request.form.get("message")
+
+    querySendMessage = "insert into messages(chatroom_id, user_id, name, message) values (%s, %s, %s, %s);"
+    param = (chatroom_id, user_id, name, message)
+    cursor.execute(querySendMessage, param)
+    conn.commit()
+    return jsonify(status="OK")
     pass
 
 if __name__ == "__main__": 
