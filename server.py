089e081eeabd1ff2dd0c6438e70b6a9e4cbcc40a
YLiao12 <seu_lyq@outlook.com>
2021-11-04 14:21:31 +0800

    script

--- server.py
+++ server.py
@@ -3,6 +3,12 @@ from flask import Flask
     
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+  host="18.219.7.155",       # 数据库主机地址
+  user="leo",    # 数据库用户名
+  passwd="L21516114leo"   # 数据库密码
+)
+
 @app.route("/") 
 def hello_world():
     return "Hello World!"
@@ -10,7 +16,6 @@ def hello_world():
 @app.route("/api/a3/get_chatrooms")
 def get_chatrooms():
     pass
-
 
 if __name__ == "__main__": 
     app.run()
