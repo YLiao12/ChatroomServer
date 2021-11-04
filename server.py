ce22fbad66e020cf5c10b7b0ab2b3888b858496b
YLiao12 <seu_lyq@outlook.com>
2021-11-04 17:17:55 +0800

    get message by pages script

--- server.py
+++ server.py
@@ -5,9 +5,9 @@ from flask import Flask, request, jsonify, json
 app = Flask(__name__)
 
 conn = mysql.connector.connect(
-  host="localhost",       # 数据库主机地址
-  user="dbuser",    # 数据库用户名
-  passwd="password",   # 数据库密码
+  host="localhost",       
+  user="dbuser",    
+  passwd="password",   
   database = "iems5722",
 )
 cursor = conn.cursor(dictionary = True)
@@ -35,11 +35,27 @@ def get_messages():
     # get message from mysql
     # change into json. return\
     chatroom_id = request.args.get("chatroom_id")
-    page = request.args.get("page")
+    page = int(request.args.get("page"))
     queryMessages = "select * from messages where chatroom_id = %s"
     param = (chatroom_id, )
     cursor.execute(queryMessages, param)
     results = cursor.fetchall()
+    current_msg_id = int(results[len(results) - 1]["id"])
+    print(current_msg_id)
+
+    # 
+    # select page using current_msg_id (every page 10 messages)
+    # delete key "id" and "chatroom_id" in every dict selected in results list
+
+    total_pages = current_msg_id / 10 + 1
+    if page > total_pages:
+        return jsonify(message="You are requesting more than total page ", status = "ERROR")
+
+    showing_messages = None
+    # calculating starting id and ending id
+    # for i in range ( , )
+
+
     return json.dumps(results)
     pass
 
@@ -52,6 +68,9 @@ def send_message():
     user_id = request.form.get("user_id")
     name = request.form.get("name")
     message = request.form.get("message")
+
+    if chatroom_id == None or user_id == None or  name == None or message == None:
+        return jsonify(message="Send parameters should not be None! ", status = "ERROR")
 
     querySendMessage = "insert into messages(chatroom_id, user_id, name, message) values (%s, %s, %s, %s);"
     param = (chatroom_id, user_id, name, message)
