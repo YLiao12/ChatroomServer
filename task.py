201a56dccae068b52541a91fe31ea796bff561b6
YLiao12 <seu_lyq@outlook.com>
2021-12-05 16:46:16 +0800

    push to all registered devices

--- task.py
+++ task.py
@@ -1,9 +1,18 @@
 from celery import Celery
 from flask import Flask
 from pyfcm import FCMNotification
+import mysql.connector
 
 
 app = Flask(__name__)
+
+conn = mysql.connector.connect(
+  host="localhost",       
+  user="leo",    
+  passwd="L21516114leo",   
+  database = "iems5722",
+)
+cursor = conn.cursor(dictionary = True)
 
 def make_celery(app):
     celery = Celery(
@@ -28,10 +37,25 @@ celery = make_celery(app)
 def send_push(name, message):
     push_service = FCMNotification(api_key = "AAAAViDzwi0:APA91bHFScstMcDN3e7lmUxcgr4OrDHAmcmI2dVsZkOS9QAVpdXlsUQjhoIYsS8Lj7pU2rQ9XEXCpnZmHcPZVEHN5ghOc7jYKnJdce6giENsKcj4UHlxL8pIkXpxQlkpiR-vxJJR5gcH")
     registration_id = "cHqVYDwWQ0i5tv4Lpld8iF:APA91bGm1vdTDPCklg4Au4l3nLs22GGQfwbkLtqYEDf1U_CffdNXpj7_j7ti4vXzmlO-l8Bxqi5xzYIJVUkKYO64kIzscHZhinjVBqwGD1CgIqWEHzFGO1CKDs9B9m5KWWFOrrdl-nX-"
+    
+    queryMessages = "select token from messages where user_id = %s"
+    param = ("1155161159", )
+    while True:
+        try:
+            cursor.execute(queryMessages, param)
+            break
+        except Exception:
+            conn.ping(True)
+    results = cursor.fetchall()
+    print(results)
     message_title = name
     message_body = message
-    result = push_service.notify_single_device(
-        registration_id = registration_id,
+    # result = push_service.notify_single_device(
+    #     registration_id = registration_id,
+    #     message_title = message_title,
+    #     message_body = message_body)
+    result = push_service.notify_multiple_devices(
+        registration_ids=results,
         message_title = message_title,
-        message_body = message_body)
-    pass
+        message_body = message_body
+    )
